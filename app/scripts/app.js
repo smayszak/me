729de3de715d83f464d421951a6d890a8251ab92
Steve Mayszak <stevemayszak@gmail.com>
2014-07-07 22:32:58 -0700

    yeoman angular app for my web

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name meApp
+ * @description
+ * # meApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('meApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
